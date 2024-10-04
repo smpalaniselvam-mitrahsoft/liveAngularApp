01dfe786f703a07f2d55d41b969e6248bf48d311
smpalaniselvam-mitrahsoft <palani.s@mitrahsoft.com>
2024-10-04 17:43:13 +0530

    Linkedin Configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { FacebookLoginProvider, SocialAuthService } from '@abacritt/angularx-social-login';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { OAuthService, AuthConfig } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +15,8 @@ export class AppComponent {
   public loggedIn: boolean | undefined;
   constructor(
     private authService: SocialAuthService,
+    private http:HttpClient,
+    private oauthService: OAuthService
   ){
     this.authService.authState.subscribe((user) => {
       this.user = user;
@@ -22,6 +26,11 @@ export class AppComponent {
     });
   }
   getFacebookPages(accessToken: string): void {
+this.http.get(`https://graph.facebook.com/v12.0/me/accounts?access_token=${accessToken}`).subscribe((res:any)=>{
+  console.log(res);
+  
+})
+
     const url = `https://graph.facebook.com/v12.0/me/accounts?access_token=${accessToken}`;
     
     fetch(url)
@@ -33,11 +42,27 @@ export class AppComponent {
       .catch(error => {
         console.error('Error fetching pages: ', error);
       });
+      
   }
   signInWithFB(event:string='Facebook'): void {
     if(event=='Facebook'){
       const loginOptions = { scope: 'pages_show_list,pages_read_engagement' };
       this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
     }
+  }
+  signout(): void {
+    this.authService.signOut();
+  }
+  loginWithLinkedIn() {
+    this.oauthService.initImplicitFlow();
+  }
+
+  handleLinkedInCallback() {
+    this.oauthService.loadDiscoveryDocumentAndTryLogin().then(() => {
+      if (this.oauthService.hasValidAccessToken()) {
+        const userInfo = this.oauthService.getIdentityClaims();
+        console.log('Logged in user info:', userInfo);
+      }
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SocialLoginModule, SocialAuthServiceConfig, FacebookLoginProvider, GoogleLoginProvider } from '@abacritt/angularx-social-login';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { AuthConfig, OAuthModule, OAuthService } from 'angular-oauth2-oidc';
+export const linkedInAuthConfig: AuthConfig = {
+  issuer: 'https://www.linkedin.com',
+  redirectUri: window.location.origin + '/auth/linkedin/callback',
+  clientId: '868w6zijo3wvc4', // Replace with LinkedIn client ID
+  responseType: 'code',
+  scope: 'r_liteprofile r_emailaddress',
+  showDebugInformation: true,
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    OAuthModule.forRoot()
   ],
   providers: [
     {
@@ -23,14 +34,31 @@ import { SocialLoginModule, SocialAuthServiceConfig, FacebookLoginProvider, Goog
 
           {
             id: FacebookLoginProvider.PROVIDER_ID,
-            provider: new FacebookLoginProvider('513143014764831'), //local support
+            provider: new FacebookLoginProvider('847026557578774'), //local support
+            // provider: new FacebookLoginProvider('513143014764831'), //local support
+            // provider: new FacebookLoginProvider('950085863826651'), //local support
           }
         ],
         onError: (err) => {
         }
       } as SocialAuthServiceConfig,
     },
+    OAuthService,
+    {
+      provide: AuthConfig,
+      useValue: linkedInAuthConfig,
+    },
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    private oauthService: OAuthService
+  ){
+    this.configureOAuth();
+  }
+  private configureOAuth(): void {
+    this.oauthService.configure(linkedInAuthConfig);
+    this.oauthService.setupAutomaticSilentRefresh();
+  }
+ }
