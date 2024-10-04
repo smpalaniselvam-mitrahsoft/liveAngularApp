e43a2a4b282d26a28d620b966750abe2c2389bbe
smpalaniselvam-mitrahsoft <palani.s@mitrahsoft.com>
2024-10-04 18:07:12 +0530

    Initial Project commitssss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ const linkedInAuthConfig: AuthConfig = {
   tokenEndpoint: 'https://www.linkedin.com/oauth/v2/accessToken',
   responseType: 'code',
   scope: 'r_emailaddress r_liteprofile',
-  requireHttps: false, // Set to true in production
+  requireHttps: true, // Set to true in production
   showDebugInformation: true,
 };
 @Component({
